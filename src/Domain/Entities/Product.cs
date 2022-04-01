c48c3a26b2fcbf970cac67020002db45dcd1915a
awrobel196 <52506429+awrobel196@users.noreply.github.com>
2022-04-01 20:30:27 +0200

    Update Product entity for allow nullable quantity, number and description

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -11,9 +11,9 @@ namespace Domain.Entities
     {
         [Key] public Guid Id { get; set; }
         [MaxLength(100)] public string Name { get; set; } = String.Empty;
-        public int Number { get; set; }
-        public int Quantity { get; set; }
-        [MaxLength(200)] public string Description { get; set; } = String.Empty;
+        public int? Number { get; set; }
+        public int? Quantity { get; set; }
+        [MaxLength(200)] public string? Description { get; set; } = String.Empty;
         public decimal Price { get; set; }
     }
 }
---
+++ src/Infrastructure/Persistance/Migrations/20220401182740_m1-allow-to-nullable-in-product.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistance.Migrations
+{
+    public partial class m1allowtonullableinproduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantity",
+                table: "Product",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Number",
+                table: "Product",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Product",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantity",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Number",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Product",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
