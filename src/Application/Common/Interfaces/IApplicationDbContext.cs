a454893d34f12448d727b6aa63e99871fc24d2c9
awrobel196 <52506429+awrobel196@users.noreply.github.com>
2022-04-01 19:48:12 +0200

    Add DbContext Interface

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Product> Answer { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
