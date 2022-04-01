ef1918f40be236151b895134dcb571bce1f8f13d
awrobel196 <52506429+awrobel196@users.noreply.github.com>
2022-04-01 19:40:10 +0200

    Add Product entity

---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        [Key] public Guid Id { get; set; }
+        [MaxLength(100)] public string Name { get; set; } = String.Empty;
+        public int Number { get; set; }
+        public int Quantity { get; set; }
+        [MaxLength(200)] public string Description { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+    }
+}
