121274d9b56fb8b33d89294767fb83f1c318420f
awrobel196 <52506429+awrobel196@users.noreply.github.com>
2022-04-02 15:21:19 +0200

    Add logic for get product by id

---
+++ src/Application/Products/Queries/GetProducts/GetProductByIdQuery.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Products.Queries.GetProducts
+{
+    public class GetProductByIdQuery : IRequest<ProductDto>
+    { 
+        public string Id { get; set; }
+    }
+
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ProductDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetProductByIdQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<ProductDto> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Product
+                .Where(x => x.Id == new Guid(request.Id))
+                .SingleOrDefaultAsync(cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Product), request.Id);
+            }
+
+            var product = _mapper.Map<ProductDto>(entity);
+            return product;
+        }
+    }
+}
