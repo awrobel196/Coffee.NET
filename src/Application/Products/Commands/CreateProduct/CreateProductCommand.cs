8189afa0a0851cbb255e0dbaf86da491d6c85ac4
awrobel196 <52506429+awrobel196@users.noreply.github.com>
2022-04-02 09:38:44 +0200

    Add Command for create product

---
+++ src/Application/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Application.Products.Commands.CreateProduct
+{
+    public class CreateProductCommand : IRequest<Guid>
+    {
+        public string? Name { get; set; }
+        public int Number { get; set; }
+        public int Quantity { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
