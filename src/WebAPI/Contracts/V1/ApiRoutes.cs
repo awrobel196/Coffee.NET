bdd40399fa19dffe835c4eafd0032d4df4879cd3
awrobel196 <52506429+awrobel196@users.noreply.github.com>
2022-04-02 09:29:20 +0200

    Define api routes

---
+++ src/WebAPI/Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,19 @@
+﻿namespace WebAPI.Contracts.V1
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public const string Base = $"{Root}/{Version}";
+
+
+        public static class Products
+        {
+            public const string GetAll = Base + "/products";
+            public const string GetById = Base + "/products/{id}";
+            public const string Create = Base + "/products";
+            public const string Update = Base + "/products";
+            public const string Delete = Base + "/products/{id}";
+        }
+    }
+}
