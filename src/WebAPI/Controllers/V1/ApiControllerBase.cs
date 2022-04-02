8cc38d3cf6e0ecc59d5a3e26191f093b02436d58
awrobel196 <52506429+awrobel196@users.noreply.github.com>
2022-04-02 10:22:10 +0200

    Add base API controller

---
+++ src/WebAPI/Controllers/V1/ApiControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.V1
+{
+    [ApiController]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        private ISender _mediator = null!;
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
