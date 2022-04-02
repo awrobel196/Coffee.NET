3f582e506dffb655b138e4bf8a744112d55402a9
awrobel196 <52506429+awrobel196@users.noreply.github.com>
2022-04-02 18:22:46 +0200

    Add update product logic

---
+++ src/Application/Common/Validation/UpdateProductCommandValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Products.Commands.DeleteProduct;
+using Application.Products.Commands.UpdateProduct;
+using FluentValidation;
+
+namespace Application.Common.Validation
+{
+    public class UpdateProductCommandValidation : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidation()
+        {
+            RuleFor(x => x.Description).NotNull().NotEmpty()
+                .WithMessage("he Name cannot be null or empty")
+                .MaximumLength(200).WithMessage("The Description cannot be longer than 200 characters");
+        }
+    }
+}
---
+++ src/Application/Products/Commands/UpdateProduct/UpdateProductCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Products.Commands.DeleteProduct;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Products.Commands.UpdateProduct
+{
+    public class UpdateProductCommand : IRequest<UpdateProductHelper>
+    {
+        public Guid Id { get; set; }
+        public int? Quantity { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, UpdateProductHelper>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateProductCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<UpdateProductHelper> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _context.Product.Where(x => x.Id == request.Id)
+                .SingleOrDefaultAsync(cancellationToken);
+
+            if (product == null)
+            {
+                return new UpdateProductHelper(HttpStatusCode.NotFound, $"Product with id {request.Id} not found");
+            }
+
+            product.Quantity = request.Quantity;
+            product.Description = request.Description;
+
+            if (await _context.SaveChangesAsync(cancellationToken) < 1)
+            {
+                return new UpdateProductHelper(HttpStatusCode.InternalServerError, $"Error while save changes in database");
+            }
+
+            return new UpdateProductHelper(HttpStatusCode.OK, $"");
+        }
+    }
+}
